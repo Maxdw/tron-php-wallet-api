a895c658ef7e833541fee2a853b3677b6b9f98c6
Max van Holten <max@dutchwise.nl>
2018-04-16 17:22:58 +0200

    updated genphp.sh and composer.json version

--- genphp.sh
+++ genphp.sh
@@ -3,26 +3,33 @@
 #
 # Run this script to generate the PHP GRPC client and proto data objects
 #
-# 1. Checks if grpc submodule needs initialisation
-# 2. Checks if the protoc grpc_php_plugin needs to be built using make
-# 3. Runs protoc and generates PHP GRPC client and proto data objects
+# 1. Checks if protoc is installed
+# 2. Checks if grpc submodule needs initialisation
+# 3. Checks if the protoc grpc_php_plugin needs to be built using make
+# 4. Runs protoc and generates PHP GRPC client and proto data objects
 #
+
+command -v protoc >/dev/null 2>&1 || {
+    echo >&2 "I require bin protoc @ https://github.com/google/protobuf but it is not installed, aborting"; exit 1;
+}
 
 cwd=$(pwd)
 
-if [ ! -f "./modules/grpc/Makefile" ]
+if [ ! -d "./modules/grpc/objs/opt" ]
 then
     cd ./modules/grpc
     git submodule update --init
 fi
 
-cwd=$(pwd)
+cd $cwd
 
 if [ ! -f "./modules/grpc/bins/opt/grpc_php_plugin" ]
 then
     cd ./modules/grpc
     make grpc_php_plugin
 fi
+
+cd $cwd
 
 protoc \
     --proto_path=modules/protocol \
