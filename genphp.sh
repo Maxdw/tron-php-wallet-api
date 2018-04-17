166ba8aa438fa7752bba88ad5008ba9cbadf7bd5
Max van Holten <max@dutchwise.nl>
2018-04-17 11:26:08 +0200

    updated genphp.sh to generate php files from all of tron's proto files and updated composer.json version and generated all protocol php files

--- genphp.sh
+++ genphp.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-#
 # Run this script to generate the PHP GRPC client and proto data objects
 #
 # 1. Checks if protoc is installed
 # 2. Checks if grpc submodule needs initialisation
 # 3. Checks if the protoc grpc_php_plugin needs to be built using make
 # 4. Runs protoc and generates PHP GRPC client and proto data objects
-#
 
 command -v protoc >/dev/null 2>&1 || {
     echo >&2 "I require bin protoc @ https://github.com/google/protobuf but it is not installed, aborting"; exit 1;
@@ -31,13 +29,18 @@ fi
 
 cd $cwd
 
-if protoc \
-    --proto_path=modules/protocol \
-    --grpc_out=src \
-    --plugin=protoc-gen-grpc=./modules/grpc/bins/opt/grpc_php_plugin \
-    --php_out=src \
-    ./modules/protocol/api/api.proto; then
-    echo -e "\033[33m Generated php files\033[0m"
-else
-    echo -e "\033[31m Failed to generate php files\033[0m"
-fi
+files=$(find ./modules/protocol/ -type f -name "*.proto")
+
+for f in $files
+do
+    if protoc \
+        --proto_path=modules/protocol \
+        --grpc_out=src \
+        --plugin=protoc-gen-grpc=./modules/grpc/bins/opt/grpc_php_plugin \
+        --php_out=src \
+        $f; then
+        echo -e "\033[33m Generated php files from .proto file '$f'\033[0m"
+    else
+        echo -e "\033[31m Failed to generate php files from .proto file '$f'\033[0m"
+    fi
+done
